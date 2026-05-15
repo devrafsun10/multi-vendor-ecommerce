a7a00b4fabf6e9a8138cbadc39dc88ee33816af7
devrafsun10 <ssiyam152@gmail.com>
2026-05-15 20:53:30 +0600

    User model done

--- src/models/user.js
+++ src/models/user.js
@@ -48,3 +48,21 @@ const userSchema = new Schema({
             default: Date.now
         }
 },{timestamps:true})
+
+//hashing the password brfore saving the user document to the database
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next()
+
+    const salt = await bcrypt.genSalt(12)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
+
+//compare the provided password with the hashed password in the database
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password)  
+}
+
+module.exports = mongoose.model('User', userSchema);
